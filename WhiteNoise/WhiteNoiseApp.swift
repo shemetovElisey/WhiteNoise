e70fbeb13d9ad6e8552b09ca1cd2b3ed6ae82cce
Elisey Shemetov <shemetov.elisey@wb.ru>
2025-07-05 10:14:18 +0700

    Initial Commit

---
+++ WhiteNoise/WhiteNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteNoiseApp.swift
+//  WhiteNoise
+//
+//  Created by Shemetov Elisey on 05.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhiteNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
